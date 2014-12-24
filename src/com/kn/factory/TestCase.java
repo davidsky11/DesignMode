18171454e9530d71a134fe82c276576ac6b2b612
kevin103 <kevin103@foxconn.com>
2014-12-24 17:32:58 +0800

    M factory testcase.

--- src/com/kn/factory/TestCase.java
+++ src/com/kn/factory/TestCase.java
@@ -5,8 +5,20 @@ import org.junit.Test;
 public class TestCase {
 
 	@Test
-	public void test() {
+	public void test1() {
 		IFactory f = new OracleDaoFactory();
+		UserDao userDao = f.createUserDao();
+		userDao.insert(new User());
+		userDao.deleteById("123");
+		
+		DeptDao deptDao = f.createDeptDao();
+		deptDao.insert(new Dept());
+		deptDao.deleteById("234");
+	}
+	
+	@Test
+	public void test2() {
+		IFactory f = new DB2DaoFactory();
 		UserDao userDao = f.createUserDao();
 		userDao.insert(new User());
 		userDao.deleteById("123");
